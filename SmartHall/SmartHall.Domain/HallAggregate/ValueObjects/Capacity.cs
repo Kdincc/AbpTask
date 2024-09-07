1190f8cad59cd3053d53f875abb08322b516536c
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 11:50:42 +0300

    Implementing Cost class

--- SmartHall/SmartHall.Domain/HallAggregate/ValueObjects/Capacity.cs
+++ SmartHall/SmartHall.Domain/HallAggregate/ValueObjects/Capacity.cs
@@ -9,9 +9,26 @@ namespace SmartHall.Domain.HallAggregate.ValueObjects
 {
 	public sealed class Capacity : ValueObject
 	{
+		public int Value { get; private set; }
+
+		private Capacity(int value)
+		{
+			Value = value;
+		}
+
 		public override IEnumerable<object> GetEqualityComponents()
 		{
-			throw new NotImplementedException();
+			yield return Value;
+		}
+
+		public static Capacity Create(int value)
+		{
+			if (value < 0)
+			{
+				throw new ArgumentException("Capacity cannot be negative");
+			}
+
+			return new Capacity(value);
 		}
 	}
 }
