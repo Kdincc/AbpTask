22bfd15e4470f5aeaaf7f65b79e38e207e7b29ba
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 15:28:30 +0300

    Add ReservationId

--- SmartHall/SmartHall.Domain/HallAggregate/Entities/Reservation/Reservation.cs
+++ SmartHall/SmartHall.Domain/HallAggregate/Entities/Reservation/Reservation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SmartHall.Domain.Common.Models;
+using SmartHall.Domain.HallAggregate.Entities.Reservation.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace SmartHall.Domain.HallAggregate.Entities.Reservation
 {
-    public sealed class Reservation
-    {
-    }
+	public sealed class Reservation : Entity<ReservationId>
+	{
+		public Reservation(ReservationId id) : base(id)
+		{
+		}
+	}
 }
---
+++ SmartHall/SmartHall.Domain/HallAggregate/Entities/Reservation/ValueObjects/ReservationId.cs
@@ -0,0 +1,28 @@
+﻿using SmartHall.Domain.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Domain.HallAggregate.Entities.Reservation.ValueObjects
+{
+	public sealed class ReservationId : ValueObject
+	{
+        private ReservationId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; private set; }
+
+		public override IEnumerable<object> GetEqualityComponents()
+		{
+			throw new NotImplementedException();
+		}
+
+		public static ReservationId CreateUnique() => new(Guid.NewGuid());
+
+		public static ReservationId Create(string value) => new(Guid.Parse(value));
+	}
+}
