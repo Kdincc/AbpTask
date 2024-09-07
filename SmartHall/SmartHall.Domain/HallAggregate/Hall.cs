604b0bd074e929074241f060a76f025daa68311a
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 15:26:39 +0300

    Add Reservation entity

--- SmartHall/SmartHall.Domain/HallAggregate/Hall.cs
+++ SmartHall/SmartHall.Domain/HallAggregate/Hall.cs
@@ -2,6 +2,7 @@
 using SmartHall.Domain.Common.ValueObjects;
 using SmartHall.Domain.HallAggregate.ValueObjects;
 using SmartHall.Domain.HallEqupmentAggregate;
+using SmartHall.Domain.HallEqupmentAggregate.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace SmartHall.Domain.HallAggregate
 {
     public sealed class Hall : AggregateRoot<HallId>
 	{
-		public Hall(HallId id, string name, Capacity capacity, Cost baseCost, List<HallEquipment> hallEquipment) : base(id)
+		public Hall(HallId id, string name, Capacity capacity, Cost baseCost, List<HallEquipmentId> hallEquipment) : base(id)
 		{
 			Name = name;
 			Capacity = capacity;
@@ -26,7 +27,6 @@ namespace SmartHall.Domain.HallAggregate
 
 		public Cost BaseCost { get; private set; }
 
-		public IReadOnlyCollection<HallEquipment> HallEquipment { get; private set; }
-
+		public IReadOnlyCollection<HallEquipmentId> HallEquipment { get; private set; }
 	}
 }
