fbe6dc53e3bc61ad1036e8b71d2194310ef46244
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 11:49:55 +0300

    Add Capacity class in Domain layer to HallAggregate/ValueObjects

---
+++ SmartHall/SmartHall.Domain/HallAggregate/ValueObjects/Capacity.cs
@@ -0,0 +1,17 @@
+﻿using SmartHall.Domain.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Domain.HallAggregate.ValueObjects
+{
+	public sealed class Capacity : ValueObject
+	{
+		public override IEnumerable<object> GetEqualityComponents()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
