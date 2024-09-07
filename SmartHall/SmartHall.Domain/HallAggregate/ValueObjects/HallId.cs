d00248c4de355c577e69743ed14b9426d607ab42
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 11:58:11 +0300

    Implement HallId

--- SmartHall/SmartHall.Domain/HallAggregate/ValueObjects/HallId.cs
+++ SmartHall/SmartHall.Domain/HallAggregate/ValueObjects/HallId.cs
@@ -9,9 +9,31 @@ namespace SmartHall.Domain.HallAggregate.ValueObjects
 {
 	public sealed class HallId : ValueObject
 	{
+		public Guid Value { get; private set; }
+
+		private HallId(Guid value)
+		{
+			Value = value;
+		}
+
 		public override IEnumerable<object> GetEqualityComponents()
 		{
-			throw new NotImplementedException();
+			yield return Value;
+		}
+
+		public static HallId CreateUnique()
+		{
+			return new HallId(Guid.NewGuid());
+		}
+
+		public static HallId Create(Guid value)
+		{
+			if (value == Guid.Empty)
+			{
+				throw new ArgumentException("HallId cannot be empty");
+			}
+
+			return new HallId(value);
 		}
 	}
 }
