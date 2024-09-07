f61cb6dad097ef5b969aad0f45b21db3248f74d5
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 11:21:53 +0300

    Imolement IEquatable<Entity<TId>> for Entity class

--- SmartHall/SmartHall.Domain/Common/Models/Entity.cs
+++ SmartHall/SmartHall.Domain/Common/Models/Entity.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace SmartHall.Domain.Common.Models
 {
-	public abstract class Entity
+	public abstract class Entity<TId> : IEquatable<Entity<TId>>	where TId : notnull, ValueObject
 	{
+		public TId Id { get; protected set; }
+
+		public bool Equals(Entity<TId> other)
+		{
+			return Equals(other as object);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is null or not Entity<TId>)
+			{
+				return false;
+			}
+
+			var entity = obj as Entity<TId>;
+
+			return Id.Equals(entity.Id);
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(Id);
+		}
+
+		public static bool operator ==(Entity<TId> left, Entity<TId> right)
+		{
+			return left.Equals(right);
+		}
+
+		public static bool operator !=(Entity<TId> left, Entity<TId> right)
+		{
+			return !left.Equals(right);
+		}
 	}
 }
