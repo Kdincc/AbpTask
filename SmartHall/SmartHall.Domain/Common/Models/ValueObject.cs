8a4ff0e3bd792f561ae0d0faacf96b0e4ee1e175
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-07 11:12:03 +0300

    Implement IEquatable<ValueObject> for ValueObject class

--- SmartHall/SmartHall.Domain/Common/Models/ValueObject.cs
+++ SmartHall/SmartHall.Domain/Common/Models/ValueObject.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace SmartHall.Domain.Common.Models
 {
-	public abstract class ValueObject
+	public abstract class ValueObject : IEquatable<ValueObject>
 	{
+		public abstract IEnumerable<object> GetEqualityComponents();
+
+		public bool Equals(ValueObject other)
+		{
+			return Equals(other as object);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is null or not ValueObject)
+			{
+				return false;
+			}
+
+			var valueObject = obj as ValueObject;
+
+			bool isEquantityComponentsEqual = GetEqualityComponents()
+				.SequenceEqual(valueObject.GetEqualityComponents());
+
+			return isEquantityComponentsEqual;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(GetEqualityComponents());
+		}
 	}
 }
