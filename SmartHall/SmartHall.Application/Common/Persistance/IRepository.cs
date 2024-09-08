a45bdb8a330cd0f99072b3b924ebd812cf68143b
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-08 13:31:13 +0300

    Add IRepository interface with base CRUD methods

---
+++ SmartHall/SmartHall.Application/Common/Persistance/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Application.Common.Persistance
+{
+	public interface IRepository<T> where T : class
+	{
+		public Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken);
+
+		public Task DeleteAsync(T entity, CancellationToken cancellationToken);
+
+		public Task UpdateAsync(T entity, CancellationToken cancellationToken);
+
+		public Task AddAsync(T entity, CancellationToken cancellationToken);
+	}
+}
