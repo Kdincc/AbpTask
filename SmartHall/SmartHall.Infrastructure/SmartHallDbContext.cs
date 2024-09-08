82e5843860fcc619eb5c74be11a002242269812b
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-08 10:44:54 +0300

    Add Dbcontext to infrastructure

---
+++ SmartHall/SmartHall.Infrastructure/SmartHallDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Infrastructure
+{
+	public sealed class SmartHallDbContext : DbContext
+	{
+		public SmartHallDbContext(DbContextOptions<SmartHallDbContext> options) : base(options)
+		{
+		}
+	}
+}
