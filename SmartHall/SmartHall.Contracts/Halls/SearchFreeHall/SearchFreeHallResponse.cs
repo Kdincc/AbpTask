f514ce35c7de7907ae509202d803f094869ae5b4
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-10 13:40:27 +0300

    Add SearchFreeHall request/response

---
+++ SmartHall/SmartHall.Contracts/Halls/SearchFreeHall/SearchFreeHallResponse.cs
@@ -0,0 +1,11 @@
+﻿using SmartHall.Contracts.Halls.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Contracts.Halls.SearchFreeHall
+{
+	public record SearchFreeHallResponse(List<HallDto> AvailableHalls);
+}
