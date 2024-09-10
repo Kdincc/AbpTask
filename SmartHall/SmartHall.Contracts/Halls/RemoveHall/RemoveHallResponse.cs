e5821c17ab6f7bf613d2ab0221f68329ab86432a
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-10 13:32:03 +0300

    Add RemoveHall request/response

---
+++ SmartHall/SmartHall.Contracts/Halls/RemoveHall/RemoveHallResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Contracts.Halls.RemoveHall
+{
+	public record RemoveHallResponse(Guid HallId);
+}
