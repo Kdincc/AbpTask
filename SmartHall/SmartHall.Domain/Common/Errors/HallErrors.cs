0669bd1c2601e625ead808c35064ef45697bdc54
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-10 14:49:11 +0300

    Add HallErrors class

---
+++ SmartHall/SmartHall.Domain/Common/Errors/HallErrors.cs
@@ -0,0 +1,16 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Domain.Common.Errors
+{
+	public static class HallErrors
+	{
+		public static Error HallNotFound => Error.NotFound(
+			code: "Hall.NotFound",
+			description: "Hall with that Id not found");
+	}
+}
