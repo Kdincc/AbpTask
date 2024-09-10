a9135d0632b01a60f47602ee9b27fb2413383a93
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-10 13:09:27 +0300

    Add UpdateHallRequest

---
+++ SmartHall/SmartHall.Contracts/Halls/UpdateHall/UpdateHallRequest.cs
@@ -0,0 +1,11 @@
+﻿using SmartHall.Contracts.Halls.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Contracts.Halls.UpdateHall
+{
+	public record UpdateHallRequest(Guid HallId, string Name, decimal BaseCost, List<HallEquipmentDto> HallEquipment);
+}
