da2f0fb771f4fc135a0af814805b60876bdf20ce
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-15 16:34:30 +0300

    Add ReserveHallRequestValidator

--- SmartHall/SmartHall.Application/Halls/Validators/ReserveHallRequestValidator.cs
+++ SmartHall/SmartHall.Application/Halls/Validators/ReserveHallRequestValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SmartHall.Contracts.Halls.ReserveHall;
+using SmartHall.Domain.Common.Constanst;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,33 @@ namespace SmartHall.Application.Halls.Validators
 {
     public sealed class ReserveHallRequestValidator : AbstractValidator<ReserveHallRequest>
     {
-        public ReserveHallRequestValidator()
+        public ReserveHallRequestValidator(TimeProvider timeProvider)
         {
+            RuleFor(request => request)
+                .Must(BeWithinOneDay)
+				.WithMessage("Reservation must be within one day");
+
             RuleFor(c => c.HallId)
                 .NotEmpty();
 
             RuleFor(c => c.ReservationDateTime)
-                .NotEmpty();
+                .NotEmpty()
+                .Must(c => c >= timeProvider.GetUtcNow())
+                .WithMessage("Reservation date time must be in the future")
+                .Must(c => c.TimeOfDay >= BusinessHours.OpenTime && c.TimeOfDay <= BusinessHours.CloseTime)
+                .WithMessage("Reservation time must be in range 06:00 - 23:00");
 
             RuleFor(c => c.Duratation)
-                .NotEmpty();
+                .NotEmpty()
+                .GreaterThan(TimeSpan.Zero);
         }
+
+        private bool BeWithinOneDay(ReserveHallRequest request)
+        {
+			DateTime start = request.ReservationDateTime;
+			DateTime end = start.Add(request.Duratation);
+
+			return start.Date == end.Date;
+		}
     }
 }
---
+++ SmartHall/SmartHall.Domain/Common/Constanst/BusinessHours.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Domain.Common.Constanst
+{
+	public static class BusinessHours
+	{
+		public static readonly TimeSpan OpenTime = new(6, 0, 0);
+
+		public static readonly TimeSpan CloseTime = new(23, 0, 0);
+	}
+}
