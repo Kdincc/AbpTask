c7537f2b4084d24a6e19cdb13c59bb74a4bd7824
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-15 16:48:39 +0300

    Add UpdateHallRequestValidator

---
+++ SmartHall/SmartHall.Application/Halls/Validators/SearchFreeHallRequestValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using SmartHall.Contracts.Halls.GetFreeHall;
+using SmartHall.Domain.Common.Constanst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Application.Halls.Validators
+{
+	public sealed class SearchFreeHallRequestValidator : AbstractValidator<SearchFreeHallRequest>
+	{
+		public SearchFreeHallRequestValidator(TimeProvider timeProvider)
+		{
+			RuleFor(c => c)
+				.Must(BeWithinOneDay)
+				.WithMessage("Reservation must be within one day");
+
+			RuleFor(c => c.DateTime)
+				.NotEmpty()
+				.Must(c => c >= timeProvider.GetUtcNow())
+				.WithMessage("Reservation date time must be in the future")
+				.Must(c => c.TimeOfDay >= BusinessHours.OpenTime && c.TimeOfDay <= BusinessHours.CloseTime)
+				.WithMessage("Reservation time must be in range 06:00 - 23:00");
+
+			RuleFor(c => c.Duratation)
+				.NotEmpty()
+				.GreaterThan(TimeSpan.Zero);
+
+			RuleFor(c => c.Capacity)
+				.NotEmpty()
+				.GreaterThan(0);
+		}
+
+		private bool BeWithinOneDay(SearchFreeHallRequest request)
+		{
+			DateTime start = request.DateTime;
+			DateTime end = start.Add(request.Duratation);
+
+			return start.Date == end.Date;
+		}
+	}
+}
---
+++ SmartHall/SmartHall.Application/Halls/Validators/UpdateHallRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using SmartHall.Contracts.Halls.UpdateHall;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartHall.Domain.Common.Constanst.Halls;
+
+namespace SmartHall.Application.Halls.Validators
+{
+	public sealed class UpdateHallRequestValidator : AbstractValidator<UpdateHallRequest>
+	{
+		public UpdateHallRequestValidator()
+		{
+			RuleFor(c => c.HallId)
+				.NotEmpty();
+
+			RuleFor(c => c.Name)
+				.NotEmpty()
+				.MaximumLength(HallConstants.MaxNameLenght);
+
+			RuleFor(c => c.Capacity)
+				.GreaterThan(0);
+
+			RuleFor(c => c.BaseCost)
+				.GreaterThan(0);
+		}
+	}
+}
