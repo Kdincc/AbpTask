e7a4537d7a8677d9ebb5077142b758aecad81cab
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-17 12:23:51 +0300

    Add ApiController

---
+++ SmartHall/SmartHall/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using ErrorOr;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartHall.Controllers
+{
+	[ApiController]
+	public abstract class ApiController : ControllerBase
+	{
+		protected IActionResult Problem(List<Error> errors)
+		{
+			Error error = errors.First();
+
+			int statusCode = error.Type switch
+			{
+				ErrorType.Conflict => StatusCodes.Status409Conflict,
+				ErrorType.NotFound => StatusCodes.Status404NotFound,
+				ErrorType.Validation => StatusCodes.Status401Unauthorized,
+				_ => StatusCodes.Status500InternalServerError
+			};
+
+			return Problem(statusCode: statusCode, title: error.Description);
+		}
+	}
+}
