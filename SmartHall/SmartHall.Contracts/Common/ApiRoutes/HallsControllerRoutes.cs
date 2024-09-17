a0751095326dd571489ed87b9c83228815e9202b
dermenzhi.kostya@gmail.com <dermenzhi.kostya@gmail.com>
2024-09-17 12:26:59 +0300

    Add HallsControllerRoutes

---
+++ SmartHall/SmartHall.Contracts/Common/ApiRoutes/HallsControllerRoutes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHall.Contracts.Common.ApiRoutes
+{
+	public static class HallsControllerRoutes
+	{
+		public const string CreateHall = "halls/create";
+
+		public const string RemoveHall = "halls/remove";
+
+		public const string UpdateHall = "halls/update";
+
+		public const string SearchFreeHall = "halls/search";
+	}
+}
